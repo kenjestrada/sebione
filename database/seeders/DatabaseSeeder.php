c63ff4ea1f2d94fd19b8fa29168c22df324f53fc
KENNETH JOHN ESTRADA <qkjsestrada@tip.edu.ph>
2022-10-22 19:12:43 +0800

    Added first user with email:admin@admin.com and password:password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create(
+            ['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt('password')]
+        );
+        
     }
 }
